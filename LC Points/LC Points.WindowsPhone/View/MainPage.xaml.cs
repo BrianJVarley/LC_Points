3150694613034ad9ec956a81d79571df0fbf7c82
BrianJVarley <brianvarley9@gmail.com>
2015-11-09 12:37:19 +0000

    Refactored share task to Main Page app bar button click. Deleted PostPage in place of message dialog confirmations on MainPage.

--- LC Points/LC Points.WindowsPhone/View/MainPage.xaml.cs
+++ LC Points/LC Points.WindowsPhone/View/MainPage.xaml.cs
@@ -1,12 +1,16 @@
-﻿using LC_Points.View;
+﻿using Facebook;
+using LC_Points.View;
 using LC_Points.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Security.Authentication.Web;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -56,7 +60,65 @@ namespace LC_Points
         }
 
 
-        
+        private async void shareBtn_Click(object sender, RoutedEventArgs e)
+        {
+            await ShareScore();
+        }
+
+
+
+        private async Task ShareScore()
+        {
+            //Facebook app id
+            var clientId = "573586446116744";
+            //Facebook permissions
+            var scope = "public_profile, email";
+
+            MessageDialog successMsgbox;
+            MessageDialog errorMsgbox = null;
+
+            var redirectUri = WebAuthenticationBroker.GetCurrentApplicationCallbackUri().ToString();
+            var fb = new FacebookClient();
+            var loginUrl = fb.GetLoginUrl(new
+            {
+                client_id = clientId,
+                redirect_uri = redirectUri,
+                response_type = "token",
+                scope = scope
+            });
+
+            Uri startUri = loginUrl;
+            Uri endUri = new Uri(redirectUri, UriKind.Absolute);
+
+            WebAuthenticationBroker.AuthenticateAndContinue(startUri, endUri, null, WebAuthenticationOptions.None);
+
+
+            Exception exception = null;
+
+            try
+            {
+                var postArgs = new Dictionary<string, string>();
+                postArgs["link"] = "http://allaboutwindowsphone.com/software/developer/Brian-Varley.php";
+                postArgs["name"] = "More from BV Apps..";
+                postArgs["message"] = "I'm using LC Points to calculate my Leaving Cert Points!";
+                await fb.PostTaskAsync("[pageid]/feed/", postArgs);
+                successMsgbox = new MessageDialog("Shared on your timeline!");
+                await successMsgbox.ShowAsync();
+            }
+            catch (Exception e)
+            {
+                errorMsgbox = new MessageDialog("An unhandled exception of type: " + e.InnerException);
+            }
+
+            if (exception != null)
+            {
+                await errorMsgbox.ShowAsync();
+            }
+
+
+        }
+
+            
         
     }
 }
